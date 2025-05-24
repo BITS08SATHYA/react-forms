f7650e3b5e34fe7b85f09bffc91ac90f35f76349
Sathya <neelasathya06@gmail.com>
2025-05-24 23:51:58 +0530

    common issues in basic forms

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,40 @@ function App() {
 
     })
 
+    const [errors, setErrors] = useState({})
+
+    const validate = () => {
+        const newErrors = {};
+        if (!formData.name.trim()){
+            newErrors.name = 'Name is required!'
+        }
+        return newErrors;
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('FormData submitted: ' , formData)
-
+        const validationErrors = validate()
+        if(Object.keys(validationErrors).length > 0){
+            setErrors(validationErrors)
+        }else{
+            console.log('FormData submitted: ' , formData)
+        }
     }
 
     const handleChange = (e) => {
         // e.preventDefault()
         // console.log(formData.name)
+        const {name, value} = e.target
         setFormData({
             ...formData,
             [e.target.name]: e.target.value,
         })
+
+        if(errors[name]){
+            const newErrors = {...errors};
+            delete newErrors[name]
+            setErrors(newErrors)
+        }
     }
 
   return (
@@ -37,6 +58,7 @@ function App() {
                     value={formData.name}
                     onChange={handleChange}
                     />
+                {errors.name && <span style={{color:'red'}}>{errors.name}</span>}
             </label>
 
             <label>
